99baa5a08527a9a8785e547d937e2d76f2090a8d
E. Anne Chambers <eachambers@utexas.edu>
2023-02-08 10:57:25 -0800

    add raw data

--- R/MVZlibrary_palettes.R
+++ R/MVZlibrary_palettes.R
@@ -108,3 +108,90 @@ test_pal(no_to_keep = 7, col_palette = Ensatina)
 test_pal(no_to_keep = 7, col_palette = WesternBirds)
 
 
+# Test dataset ------------------------------------------------------------
+
+library(tidyverse)
+library(rgdal)
+library(cowplot)
+# devtools::install_github("hrbrmstr/waffle")
+# renv::deactivate() # THIS IS THE KEY LINE FOR RACTIVATE.R ERROR
+library(waffle)
+theme_set(theme_cowplot())
+
+# Species list, grouped by taxon? https://irma.nps.gov/NPSpecies/
+# Stacked bars? x is taxonomic group, color is national park
+# For six national parks across the state
+
+dat <- read_csv("ds2739/NPSpecies_FullListWithDetails_CHIS_20230208103035.csv", col_names = TRUE)
+
+dat <-
+  dat %>%
+  dplyr::filter(Nativeness == "Native",
+                Occurrence == "Present")
+
+#' Process NPS species list data
+#'
+#' @param data path to csv file with data
+#' Data files obtained from https://irma.nps.gov/NPSpecies/
+#' Make sure you download the "Full list with details"
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter_NPS <- function(data) {
+  verts <- c("Mammal", "Amphibian", "Bird", "Fish", "Reptile")
+
+  dat <-
+    read_csv(data, col_names = TRUE) %>%
+    dplyr::filter(Nativeness == "Native",
+                  Occurrence == "Present") %>%
+    dplyr::filter(Category %in% verts)
+
+  return(dat)
+}
+
+CHIS <- filter_NPS("ds2739/NPSpecies_FullListWithDetails_CHIS_20230208103035.csv")
+JOTR <- filter_NPS("ds2739/NPSpecies_FullListWithDetails_JOTR_20230208102947.csv")
+DEVA <- filter_NPS("ds2739/NPSpecies_FullListWithDetails_DEVA_20230208103016.csv")
+SEKI <- filter_NPS("ds2739/NPSpecies_FullListWithDetails_SEKI_20230208102934.csv")
+YOSE <- filter_NPS("ds2739/NPSpecies_FullListWithDetails_YOSE_20230208102941.csv")
+
+# Bind together dataset
+dat <- bind_rows(CHIS, JOTR, DEVA, SEKI, YOSE)
+
+# Grouped bar plot based on park
+dat %>%
+  ggplot() +
+  geom_bar(aes(y = Category, fill = `Park Name`), position = "dodge")
+
+# Categorize parks based on State Status (if data available)
+dat %>%
+  filter(!is.na(`State Status`)) %>%
+  ggplot() +
+  geom_bar(aes(y = `Park Name`, fill = `State Status`))
+
+
+# Bat monitoring: https://irma.nps.gov/DataStore/Collection/Profile/7982
+# https://irma.nps.gov/DataStore/DownloadFile/639394
+
+bat_dat <- read_tsv("ds2739/JOTR_bat_data.txt", col_names = TRUE)
+
+palms <- bat_dat %>% filter(location == "49_Palms")
+
+# waffle(palms, rows = 4)
+
+waf <-
+  bat_dat %>%
+  ggplot(aes(fill = species, values = no_inds)) +
+  expand_limits(x = c(0,0), y = c(0,0)) +
+  coord_equal() +
+  labs(fill = NULL, colour = NULL) +
+  theme_void() +
+  facet_wrap(~location, ncol = 1, strip.position = "left")
+
+waf +
+  geom_waffle(n_rows = 4, size = 0.33, colour = "white")
+
+# "ds2739/ds2739_TerrestrialBiodiversitySummary_ACE.lyr"
+
---
+++ data-raw/DATASET.R
@@ -0,0 +1,13 @@
+# Code to create example data
+
+# Raw data files are from:
+
+
+# Bats at Joshua Tree NP --------------------------------------------------
+
+# liz_vcf <- vcfR::read.vcfR("inst/extdata/populations_r20.haplotypes.filtered_m70_randomSNP.vcf")
+
+# Species lists from six national parks -----------------------------------
+
+
+# usethis::use_data(bat_dat, overwrite = TRUE)
